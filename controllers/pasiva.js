5f9a9b94fe5851035be8e4aa51325897a5260f05
Nicolas Castiglioni <atilioncb@gmail.com>
2022-09-16 18:17:27 -0300

    .eliminar

--- controllers/pasiva.js
+++ controllers/pasiva.js
@@ -9,9 +9,14 @@ exports.comidas = async (req, res) => {
   const { rows } = await client.query("SELECT comida_id, comida, categoria, descripcion, precio, oferta FROM la_pasiva");
   return res.send(rows);
 };
-
+//↑ falta el llamado where eliminado = false
 exports.pedidos = async (req, res) => {
   const { rows } = await client.query("SELECT pedido_id, fecha_pedido, entregado, usuario_id la_pasiva_pedidos");
+  return res.send(rows);
+  //↑ falta el llamado where eliminado = false
+};
+exports.eliminar=async(req,res)=>{
+  const {rows}=await client.query(`UPDATE public.la_pasiva SET eliminado=true WHERE id=$1;`);
   return res.send(rows);
 };
 
--- routes/pasivaRoutes.js
+++ routes/pasivaRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { comidas, pedidos } = require("../controllers/pasiva");
+const { comidas, pedidos, eliminar } = require("../controllers/pasiva");
 
 router.get("/pasiva/comidas", comidas);
 router.get("/pasiva/pedidos", pedidos);
+router.delete("/pasiva/pedidos",eliminar)
 
 module.exports = router;
